18cc6dae215be8ff2f512d6d721ffd25625279d7
Oscar Antonio Cumpa Veintimilla <oscarcv_04112000@hotmail.com>
2022-01-21 23:33:45 -0500

    avanze de ContratoFragment.kt y se ha mejorado el diseño de activity_caja.xml (pendiente el cierre de caja)

--- app/src/main/java/dev/franklinbg/sedimobile/fragment/ContratoFragment.kt
+++ app/src/main/java/dev/franklinbg/sedimobile/fragment/ContratoFragment.kt
@@ -1,12 +1,13 @@
 package dev.franklinbg.sedimobile.fragment
 
 import android.os.Bundle
+import android.text.InputFilter
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import dev.franklinbg.sedimobile.R
 import dev.franklinbg.sedimobile.databinding.FragmentContratoBinding
+import dev.franklinbg.sedimobile.utils.DecimalDigitsInputFilter
 
 class ContratoFragment : Fragment() {
     private lateinit var binding: FragmentContratoBinding
@@ -14,7 +15,12 @@ class ContratoFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentContratoBinding.inflate(inflater,container,false)
+        binding = FragmentContratoBinding.inflate(inflater, container, false)
+        initListeners()
         return binding.root
+    }
+
+    private fun initListeners() {
+        binding.edtTotal.setFilters(arrayOf(DecimalDigitsInputFilter(2, 2)))
     }
 }
